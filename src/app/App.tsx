1f162829c6f6d53f2b18656b8cc0adec70a087d9
alexanderpono <brain16383@yandex.ru>
2025-09-22 23:33:06 +0300

    feat: add profile text

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,9 +7,25 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Текст писать тут
-        </p>
+        <p>Александр Пономаренко</p>
+        <table className='tbl'>
+          <tr>
+            <td>О себе:</td>
+            <td>Frontend-разработчик React. <br />Проходил курс "Developer React.js OTUS" 4 года назад</td>
+          </tr>
+          <tr>
+            <td>Технологии:</td>
+            <td>Применяю React, TypeScript, Webpack, Redux, Jest, Storybook</td>
+          </tr>
+          <tr>
+            <td>Цель обучения:</td>
+            <td>актуализировать знания и навыки разработки на React, Next.js</td>
+          </tr>
+          <tr>
+            <td>Telegram:</td>
+            <td><a href="https://t.me/brain16383" target="tg">https://t.me/brain16383</a></td>
+          </tr>
+        </table>
       </header>
     </div>
   );
